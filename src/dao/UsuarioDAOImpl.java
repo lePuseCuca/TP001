c5b19f1b5dcf22517b7431fb9bdae67e9d746b97
Martin Freitas <freitasmartin@yahoo.com.ar>
2021-10-21 16:46:32 -0300

    Se agrega findAll y ToUsuario

--- src/dao/UsuarioDAOImpl.java
+++ src/dao/UsuarioDAOImpl.java
@@ -1,15 +1,35 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 
+import turismoTM.ErrorDatosException;
+import turismoTM.Tipo;
 import turismoTM.Usuario;
 
 public class UsuarioDAOImpl implements UsuarioDAO{
 
 	@Override
 	public List<Usuario> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			String sql = "SELECT * FROM USUARIOS";
+			Connection conn = ConnectionProvider.getConnection();
+			PreparedStatement statement = conn.prepareStatement(sql);
+			ResultSet resultados = statement.executeQuery();
+
+			List<Usuario> usuarios = new LinkedList<Usuario>();
+			while (resultados.next()) {
+				usuarios.add(toUsuario(resultados));
+			}
+
+			return usuarios;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
 	}
 
 	@Override
@@ -40,6 +60,13 @@ public class UsuarioDAOImpl implements UsuarioDAO{
 	public Usuario findUsuarioByNombre(String nombre) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	private Usuario toUsuario(ResultSet resultados) throws SQLException, ErrorDatosException {
+		return new Usuario(resultados.getString("nombre"), 
+				resultados.getDouble("presupuesto"),
+				resultados.getDouble("tiempo"),
+				Tipo.valueOf(resultados.getString("tipo_preferido")));
 	}
 
 }
